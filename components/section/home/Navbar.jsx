b3f0afbd9ae8233e2b08d035b45e64ec75b889ab
YogurtFusion <cocaniket2@gmail.com>
2026-05-21 23:27:05 +0530

    improved navbar

--- components/section/home/Navbar.jsx
+++ components/section/home/Navbar.jsx
@@ -4,36 +4,31 @@ import NavBtn from "../../ui/NavBtn";
 
 const navLinks = [
   {
-    id:"ID1",
-    title:"home",
-    link:"/",
+    id: "ID1",
+    title: "home",
+    link: "/",
   },
   {
-    id:"ID2",
-    title:"our work",
-    link:"",
+    id: "ID2",
+    title: "our work",
+    link: "/projects",
   },
   {
-    id:"ID3",
-    title:"services",
-    link:"",
+    id: "ID3",
+    title: "services",
+    link: "/#services",
   },
   {
-    id:"ID4",
-    title:"",
-    link:"",
+    id: "ID4",
+    title: "About us ",
+    link: "/about",
   },
   {
-    id:"ID5",
-    title:"",
-    link:"",
+    id: "ID5",
+    title: "contact us",
+    link: "/contact",
   },
-  {
-    id:"ID6",
-    title:"",
-    link:"",
-  },
-]
+];
 
 const Navbar = () => {
   return (
@@ -41,48 +36,28 @@ const Navbar = () => {
       {/* logo */}
       <Link href={"/"} className=" group flex  items-center gap-3 ">
         <div className=" flex items-center justify-center text-xs font-serif w-9 h-9 rounded-full border border-neutral-900 group-hover:scale-105 transition-transform  ">
-           YG
+          YG
         </div>
         {/* logo-text */}
-        <span className="  font-light  tracking-tight hidden md:block text-sm md:text-lg leading-tight  ">
-          Yellow Glow Studios
+        <span className="  font-light  tracking-tight flex text-sm md:text-lg leading-tight  flex-col">
+          Yellow Glow <span className="lg:hidden ">Studios</span>
         </span>
       </Link>
-      <ul className=" hidden md:flex items-center justify-between gap-8  text-sm font-medium text-neutral-600 ">
-        <li>
-          <Link href={"/"} className="hover:text-black transition-colors">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href={"/projects"} className="hover:text-black transition-colors">
-            Live Sites
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={"/#services"}
-            className="hover:text-black transition-colors"
-          >
-            Services
-          </Link>
-        </li>
-        <li className="hidden md:block">
-          <Link href={"/about"} className="hover:text-black transition-colors">
-            About Us
-          </Link>
-        </li> 
-        <li className="hidden md:block">
-          <Link href={"/contact"} className="hover:text-black transition-colors">
-            contact Us
-          </Link>
-        </li> 
-       
-         
-     
+      <ul className=" hidden md:flex items-center justify-between gap-4 lg:gap-8">
+        {navLinks.map((item) => (
+          <li className="relative group" key={item.id}>
+            <Link
+              href={item.link}
+              className="inline-block text-xs lg:text-sm font-medium text-neutral-900 capitalize pb-1"
+            >
+              {item.title}
+            </Link>
+            <span className="absolute bg-black  group-hover:scale-100 opacity-0 group-hover:opacity-100 origin-left scale-0  left-0 duration-500 transition-all h-px w-full bottom-1"></span>
+          </li>
+        ))}
       </ul>
       <div>
-      <NavBtn />
+        <NavBtn />
       </div>
     </nav>
   );
